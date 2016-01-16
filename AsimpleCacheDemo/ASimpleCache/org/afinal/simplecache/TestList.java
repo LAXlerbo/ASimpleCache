eb9f1090d8ff6eca76772016511cf13eb5706367
neo001 <615951217@qq.com>
2016-01-16 23:21:08 +0800

    TestList.java
    提交list辅助类

---
+++ AsimpleCacheDemo/ASimpleCache/org/afinal/simplecache/TestList.java
@@ -0,0 +1,9 @@
+package org.afinal.simplecache;
+
+import java.io.Serializable;
+import java.util.List;
+
+public @SuppressWarnings("serial")
+class TestList<T> implements Serializable{
+	public List<T> list;
+}
